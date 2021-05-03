e7e1681e7da8100627ae28ec63f1bb5ecc7229cb
Tommy Munson <Tmunson50@MacBook-Air.attlocal.net>
2021-05-02 20:19:03 -0500

    Edited HTML to show visual

--- logic.js
+++ logic.js
@@ -44,7 +44,8 @@ function createMap(earthquakes) {
 
   // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-    "access_token= {API_KEY}");
+  // Note that the token below is a public key via Mapbox
+    "access_token= pk.eyJ1IjoidGptLXRnYyIsImEiOiJja283d3BlcDUwYW0zMndsazl2bnVnczB5In0.l26EgCcrRbGBk9ZmxmCeUw");
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
